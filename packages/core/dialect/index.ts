2034ca65f7dfc2b36d8a218d601c95343eeb6843
Matheus Teixeira <matheus.mtxr@gmail.com>
2019-05-07 21:49:52 -0300

    how-to-add-new-dialect: How to add new dialect example

--- packages/core/dialect/index.ts
+++ packages/core/dialect/index.ts
@@ -6,6 +6,7 @@ import PostgreSQL from './pgsql';
 import SQLite from './sqlite';
 import SAPHana from './saphana';
 import CQL from './cql';
+import ExampleDialect from './example-dialect';
 
 const dialects = {
   DB2,
@@ -18,6 +19,7 @@ const dialects = {
   SQLite,
   SAPHana,
   'Cassandra': CQL,
+  ExampleDialect, // add your dialect here to make it availeble for usage
 };
 
 export default dialects;
